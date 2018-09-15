0bce32ebce5e30f2e113550fa1c7f160a2c69d39
Edward Poon <edward.poon@hypr.com>
2018-09-15 12:50:28 -0400

    first commit

---
+++ SeeIfICanPushToGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SeeIfICanPushToGithub
+//
+//  Created by Edward Poon on 9/15/18.
+//  Copyright © 2018 HYPR Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SeeIfICanPushToGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SeeIfICanPushToGithub
+//
+//  Created by Edward Poon on 9/15/18.
+//  Copyright © 2018 HYPR Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
